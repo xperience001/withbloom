566f8a0c5be45d830b82948a3b795c95722e676d
xperience001 <tobilobatijani2011@gmail.com>
2023-03-22 10:07:14 +0100

    feature-flag

--- src/index.js
+++ src/index.js
@@ -4,13 +4,33 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import Bugsnag from '@bugsnag/js'
+import BugsnagPluginReact from '@bugsnag/plugin-react'
+
+Bugsnag.start({
+  apiKey: 'f5c76b3e5e7db101c12c1ef92f3b3887',
+  plugins: [new BugsnagPluginReact()]
+})
+
+const ErrorBoundary = Bugsnag.getPlugin('react')
+  .createErrorBoundary(React)
+
+  Bugsnag.notify(new Error('Test error'))
+
+  const ErrorView = () =>
+  <div>
+    <p>Inform users of an error in the component tree.</p>
+  </div>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ErrorBoundary FallbackComponent={ErrorView}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ErrorBoundary>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
